6c0f3ad44d440599acd4b7a69c73051e07cf9443
Ibrahim Khawar <ibrahim@Ibrahims-MBP.home>
2021-05-12 18:41:17 -0400

    Added functionality to all buttons and input

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-let answerArray = []
-funtion calculate {
-   
+const onButtonClick = (e) => {
+     const input = document.getElementById('NumInput');
+     let value = e.innerHTML.toString();
+     if (value === '*' || value === '' || value === '-' || value === '+') {
+          value = ' ' + value + ' ';
+     }
+     input.value += value;
+}
+
+const calculate = () => {
+     const input = document.getElementById('NumInput');
+     const value = eval(input.value);
+     input.value = value;
+}
